aaf6e6e2a525c88d87ee3eba5ce80b57cf7fd5c5
urivat <rashad.porter18@gmail.com>
2022-04-07 17:20:36 -0400

    added songs to the database with django site and created new song in postman + created Library details to get items by id

--- music/views.py
+++ music/views.py
@@ -4,10 +4,23 @@ from rest_framework.response import Response
 from music import serializer
 from music.models import Music_library
 from music.serializer import MusicSerializer
+from rest_framework import status
+from django.http import Http404
 # Create your views here.
 class LibraryList(APIView):
     def get(self,request, format=None):
         music_query = Music_library.objects.all()
         serializer= MusicSerializer(music_query, many=True)
         return Response(serializer.data)
-    
+    def post(self,request, format=None):
+        serializer = MusicSerializer(data= request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+class LibraryDetails(APIView):
+    def get_by_id(self, pk):
+        try:
+            return Music_library.objects.get(pk=pk)
+        except Music_library.DoesNotExist:
+            raise Http404
