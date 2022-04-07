a7a00bf41007f126ab9c25b7798318c219238d49
urivat <rashad.porter18@gmail.com>
2022-04-07 16:42:24 -0400

    added library list to display all music library objects fixed urls paths

--- music/urls.py
+++ music/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.LibraryDetails.as_view()),
+    path('', views.LibraryList.as_view()),
+    path('<int:pk>/',views.LibraryDetails.as_view()),
     
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from music import serializer
+from music.models import Music_library
 from music.serializer import MusicSerializer
 # Create your views here.
-class LibraryDetails(APIView):
+class LibraryList(APIView):
     def get(self,request, format=None):
-        return Response('hello')
+        music_query = Music_library.objects.all()
+        serializer= MusicSerializer(music_query, many=True)
+        return Response(serializer.data)
+    
