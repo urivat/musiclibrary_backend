bda1b20d9db89ec0e7344b753718603bf1140482
urivat <rashad.porter18@gmail.com>
2022-04-07 18:07:32 -0400

    added like_songs app and added to the music library model with a foreign key field.

--- music/models.py
+++ music/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from like_songs.models import LikeSong
+
 # Create your models here.
 class Music_library(models.Model):
     title = models.CharField(max_length=255)
@@ -7,3 +9,4 @@ class Music_library(models.Model):
     album = models.CharField(max_length=255)
     release_date = models.DateField()
     genre = models.CharField(max_length=255)
+    like_status = models.ForeignKey(LikeSong)
--- music/views.py
+++ music/views.py
@@ -33,11 +33,12 @@ class LibraryDetails(APIView):
         serializer= MusicSerializer(songs, request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk, format=None):
         songs = self.get_by_id(pk)
         songs.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_200_OK)
         
